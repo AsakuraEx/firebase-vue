d6174b37af0157dfcb626fe7e6aa30f4b70cb995
Fescobar28 <franescobar97@hotmail.com>
2024-10-11 14:54:04 -0600

    Vista del login finalizada

---
+++ src/views/LoginView.vue
@@ -0,0 +1,74 @@
+<script setup>
+    import { useForm, useField } from 'vee-validate';
+    import { loginSchema as validationSchema } from '@/validation/loginSchema';
+
+    const { handleSubmit } = useForm({validationSchema})
+
+    const email = useField('email')
+    const password = useField('password')
+
+    const submit = handleSubmit(()=>{
+        console.log("submit...")
+    })
+
+
+
+
+
+</script>
+
+<template>
+
+    <v-card
+        flat
+        max-width="600"
+        class="mx-auto my-10"
+    >
+        <v-card-title
+            class="text-h4 font-weight-bold"
+            tag="h3"
+        >
+            Iniciar Sesión
+        </v-card-title>
+        <v-card-subtitle
+            class="text-h5"
+        >
+            Inicia sesión con tu cuenta
+        </v-card-subtitle>
+
+        <v-form>
+            <v-text-field 
+                type="email"
+                label="Email"
+                bg-color="blue-grey-lighten-5"
+                variant="outlined"
+                class="mb-4"
+                v-model="email.value.value"
+                :error-messages="email.errorMessage.value"
+            />
+            <v-text-field 
+                type="password"
+                label="Contraseña"
+                bg-color="blue-grey-lighten-5"
+                hint="Ingresa la contraseña de tu cuenta para acceder al sitio"
+                variant="outlined"
+                class="mb-4"
+                v-model="password.value.value"
+                :error-messages="password.errorMessage.value"
+            />
+            <v-btn
+                density="default"
+                color="pink-accent-3"
+                block
+                @click="submit"
+            >
+                Iniciar Sesión
+            </v-btn>
+    
+        </v-form>
+    </v-card>
+
+
+
+</template>
+
